ff90aee1b357252655a203ba93989ead4584298b
kartikeya <kartikey@buffalo.edu>
2018-10-05 05:30:59 -0400

    A* in-progress
    Signed-off-by: kartikeya <kartikey@buffalo.edu>

--- pacman/pacmanAgents.py
+++ pacman/pacmanAgents.py
@@ -11,7 +11,7 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 #BFSAgent
-#A*_Agent
+#A*_Agent with in_house priority queue
 
 from pacman import Directions
 from game import Agent
